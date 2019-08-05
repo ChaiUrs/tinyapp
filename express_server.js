c837d14066e54ff8ce908b5decdf71c47bb6aeca
ChaiUrs <mysorechaitra@gmail.com>
2019-08-05 22:34:01 +0000

    Sending HTML and rendered in the client browser

--- express_server.js
+++ express_server.js
@@ -22,3 +22,23 @@ app.get("/urls.json", (req, res) => {
 
 //In the browser => http://localhost:8080/urls.json => 
 //We expect to see a JSON string representing the entire urlDatabase object
+
+
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+/*
+node express_server.js
+Example app listening on port 8080!
+vagrant [tinyapp]> curl -i http://localhost:8080/hello
+HTTP/1.1 200 OK
+X-Powered-By: Express
+Content-Type: text/html; charset=utf-8
+Content-Length: 45
+ETag: W/"2d-+kq4PwugtS0rt17Ooq6yKzvojSE"
+Date: Mon, 05 Aug 2019 22:28:24 GMT
+Connection: keep-alive
+
+<html><body>Hello <b>World</b></body></html>
+*/
