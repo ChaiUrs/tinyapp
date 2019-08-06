b41c45ac59d71df011eb7c1dbe315f6fe675f2b7
ChaiUrs <mysorechaitra@gmail.com>
2019-08-06 19:05:31 +0000

    Updated index.ejs template with POST method and added POST routing on server that deletes a URL resource

--- express_server.js
+++ express_server.js
@@ -46,6 +46,11 @@ app.post("/urls", (req, res) => {
   res.redirect(`/urls/${newRandomURL}`);
 });
 
+app.post('/urls/:shortURL/delete', (req, res) => {
+  delete urlDatabase[req.params.shortURL];
+  res.redirect('/urls')
+});
+
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
